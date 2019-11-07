101120d249f639d7aecd18b94c6e176efd273df1
Jacob Fuehne <jacobfuehne@yahoo.com>
2019-11-06 21:51:11 -0600

    uploading revised version to github
    as I said before, in the future, I will have better commit messages :+1:

--- redaction/redaction.cpp
+++ redaction/redaction.cpp
@@ -6,57 +6,80 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <sstream> 
 using namespace std;
 
-//int argc, char **argv
-int main(){
+vector<string> parse(string input, char c);
+//expected runtime format is
+//   ./redac input.xml output.txt --clearance SCI,TS,S,CNF
+int main(int argc, char *argv[]){
+	if(argc == 1){
+		cout << "Error: must include input xml file" << endl;
+	}
+	string inputFName = argv[2];
+	string outputFName = argv[3];
+	ifstream inputXML(inputFName);
+	if (inputXML.is_open()) {
+		cout << "Input file loaded successfully" << endl;
+		inputXML.close();
+	}
+	ofstream output(outputFName);
+	bool clearance = false;
+	string clearances = "";
+	//get vector of the clearances that this person has access to.
+	for(int i = 0; i < argc; i++){
+		string temp = argv[i];
+		if (clearance) {
+			clearances = temp;
+		}
+		if (temp.find("--clearance") != string::npos) {
+			clearance = true;
+		}
+	}
+	
+	std::vector<string> clearancesV = parse(clearances,',');
 	int wordL = 5;
 	int wordsL = 11;
 	int sentenceL = 90;
 	int sentencesL = 181;
 	int paragraphL = 363;
-	string levels[] = { "S", "CNF" };
+	
 	string line;
-	ifstream myfile("example.xml");
-	ofstream fout("output.xml");
-	if (myfile.is_open()){
-		while (getline(myfile, line)){
-			cout << line << '\n';
-		}
-		myfile.close();
-	} else 
-		cout << "Unable to open file";
+	//if (inputXML.is_open()){
+	//	while (getline(inputXML, line)){
+	//		cout << line << '\n';
+	//	}
+	//	inputXML.close();
+	//} 
+	//else 
+	//	cout << "Unable to open file";
 
 	pugi::xml_document doc;
-	std::string namepanel;
-	
-	pugi::xml_parse_result result = doc.load_file("example.xml");
+
+	char* fchar = new char[inputFName.size() + 1];
+	inputFName.copy(fchar, inputFName.size() + 1);
+	fchar[inputFName.size()] = '\0';
+	pugi::xml_parse_result result = doc.load_file(fchar);
 	if (result) {
-		cout << "success" << endl;
-	}
+		cout << "Successfully loaded document into XML parser" << endl;
+	} 
 	else {
-		cout << "fail" << endl;
+		cout << "Error: failed to parse XML document" << endl;
 	}
-	//pugi::xml_node project = doc.child("document");
 
-	//std::cout << "Project name: " << project.child("name").text().get() << std::endl;
-	//std::cout << "Project version: " << project.child("version").text().as_double() << std::endl;
-	//std::cout << "Project visibility: " << (project.child("public").text().as_bool(/* def= */ true) ? "public" : "private") << std::endl;
-	//std::cout << "Project description: " << project.child("description").text().get() << std::endl;
-
+	//create a node for the text of the document
 	pugi::xml_node text = doc.child("document").child("text");
-	//pugi::xml_node redact = doc.child("document").child("text").child("redact");
-	//std::cout << redact.text().get() << std::endl;
 
-
+	//parse through redactions
 	for (pugi::xml_node redact = text.child("redact"); redact; redact = redact.next_sibling("redact"))
 	{
 		int replaceLength = 0;
 		bool shouldRedact = true;
-		for (pugi::xml_attribute attr = redact.first_attribute(); attr; attr = attr.next_attribute())
-		{
+		for (pugi::xml_attribute attr = redact.first_attribute(); attr; attr = attr.next_attribute()){
 			std::string name = attr.name();
 			std::string value = attr.value();
+			//assign length 
 			if (name == "type") {
 				if (value == "Word") {
 					replaceLength = wordL;
@@ -76,42 +99,54 @@ int main(){
 			}
 			if (name == "level") {
 				//int i = std::find(std::begin(levels), std::end(levels), attr.name());
-				if (std::end(levels) != std::find(std::begin(levels), std::end(levels), value)) {
+				if (std::end(clearancesV) != std::find(std::begin(clearancesV), std::end(clearancesV), value)) {
 					shouldRedact = false;
 				}
 			}
 		}
 		if (!shouldRedact) {
-			std::cout << "redact:";
-			std::cout << redact.text().get();
-			std::cout << "you can see me" << std::endl;
+			//std::c/*out << "redact:";
+			//std::cout << redact.text().get();
+			//std::cout << "you can see me" << std::endl;*/
 			//std::cout << " " << attr.name() << "=" << attr.value() << std::endl;
 		}
 		else if (shouldRedact) {
-			std::cout << "value" << redact.text() << std::endl;
+			//create string of Full Block characters of length replaceLength
+			//must be converted to char * array because pugixml will not accept std::string
 			string redacted = string(replaceLength, 'X');
 			char *cstr = new char[replaceLength + 1];
 			redacted.copy(cstr, redacted.size() + 1);
 			cstr[redacted.size()] = '\0';
-			std::cout << replaceLength << std::endl;
-			std::cout << cstr << std::endl;
 			redact.text().set(cstr);
-			std::cout << "value" << redact.text() << std::endl;
 		}
 	}
+	//create a text file from our redacted pugixml object
 	for (pugi::xml_node_iterator it = text.begin(); it != text.end(); ++it)
 	{
-		std::cout << "text:";
-		std::cout << it->text().get();
-		fout << it->text().get();
-		std::cout << std::endl;
+		//std::cout << "text:";
+		//std::cout << it->text().get();
+		output << it->text().get();
+		//std::cout << std::endl;
 	}
-	fout.close();
+	output.close();
+	cout << "Document is finished being redacted" << endl;
 	//std::cout << "Document:\n";
 	//doc.save(std::cout);
 	//std::cout << std::endl;
 }
 
+vector<string> parse(string input, char c) {
+	stringstream stream(input);
+	vector<string> parsedVector;
+	//cout << "this is input" << input << endl;
+	while (stream.good()) {
+		string substring;
+		getline(stream, substring, ',');
+		//cout << substring << endl;
+		parsedVector.push_back(substring);
+	}
+	return parsedVector;
+}
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
