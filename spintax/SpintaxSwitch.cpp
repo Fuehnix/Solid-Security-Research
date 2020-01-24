e0ae28476a1ae6b1717d0456fd50c078ccc62e73
Jacob Fuehne <jacobfuehne@yahoo.com>
2020-01-24 15:46:38 -0600

    created template for switch class

--- spintax/SpintaxSwitch.cpp
+++ spintax/SpintaxSwitch.cpp
@@ -1,1 +1,37 @@
 #include "SpintaxSwitch.h"
+
+string SpintaxSwitch::SwitchType() {
+
+}
+
+int SpintaxSwitch::getMinVal() {
+
+}
+
+int SpintaxSwitch::getMaxVal() {
+
+}
+
+vector<string> SpintaxSwitch::getStringOptions() {
+
+}
+
+void SpintaxSwitch::setSwitchType(string type) {
+
+}
+
+void SpintaxSwitch::setMinVal(int value) {
+
+}
+
+void SpintaxSwitch::setMaxVal(int value) {
+
+}
+
+int SpintaxSwitch::spinIntValue() {
+
+}
+
+string SpintaxSwitch::spinStringValue() {
+
+}
--- spintax/SpintaxSwitch.h
+++ spintax/SpintaxSwitch.h
@@ -1,10 +1,25 @@
 #pragma once
+#include <string>
+#include <vector>
 class SpintaxSwitch
 {
 private:
 	//TO DO:
 	// Nesting...
 
-	bool switch_type;
+	string switch_type;
+	int minVal;
+	int maxVal;
+	vector<string> strings;
+public:
+	string getSwitchType();
+	int getMinVal();
+	int getMaxVal();
+	vector<string> getStringOptions();
+	void setSwitchType(string type);
+	void setMinVal(int value);
+	void setMaxVal(int value);
+	int spinIntValue();
+	string spinStringValue();
 };
 
