10ec3de61f14afc127ecefe819815b13d0f4cf31
Jacob Fuehne <jfuehne2@illinois.edu>
2020-01-27 01:44:16 -0600

    Added string, escape characters, and nesting functionality for creating the switch class.  Needs testing, and needs code implementing class to be written in the spintax.cpp

--- spintax/SpintaxSwitch.cpp
+++ spintax/SpintaxSwitch.cpp
@@ -1,23 +1,80 @@
 #include "SpintaxSwitch.h"
 
-string SpintaxSwitch::SwitchType() {
+SpintaxSwitch::SpintaxSwitch(string in) {
+	input = in;
+	if (!(in.front() == '{' && in.back() == '}')) {
+		cout << "Error: string passed is not a bounded switch statement" << endl;
+	}
+	//remove brackets from the string, leaving only the switch options.
+	in.erase(0, 1);
+	in.pop_back();
+	string modInput;
+	bool openBraces = false;
+	string substring = "";
+	//if nested is true, we know that this level must be a string switch with nesting, not integer
+	for(string::const_iterator i = in.begin(); i <= in.end(); ++i) {
+		if (!openBraces && *i == '|') {
+			strings.push_back(substring);
+			substring = "";
+			continue;
+		}
+		//if escape character, iterate forward, push it no matter what it is, then continue to the character after that
+		if (*i == '/') {
+			substring.push_back(*i);
+			++i;
+			substring.push_back(*i);
+			continue;
+		}
+		if (*i == '{') {
+			isNested = true;
+			openBraces = true;
+			substring.push_back(*i);
+			continue;
+		}
+		if (*i == '}') {
+			openBraces = false;
+			substring.push_back(*i);
+			continue;
+		}
+		if (switch_type == noType && !isdigit(*i) && *i != '-') {
+			switch_type = strType;
+			substring.push_back(*i);
+			continue;
+		}
+		if (switch_type == strType) {
+			substring.push_back(*i);
+		}
+	}
+	if (switch_type == noType) {
+		
+	}
+	stringstream stream(in);
+	
+}
 
+int SpintaxSwitch::getSwitchType(){
+	return switch_type;
 }
 
 int SpintaxSwitch::getMinVal() {
-
+	if (switch_type == intType) {
+		return minVal;
+	} else {
+		cout << "Error: Invalid switch type" << endl;
+	}
 }
 
 int SpintaxSwitch::getMaxVal() {
-
+	if (switch_type == intType) {
+		return maxVal;
+	}
+	else {
+		cout << "Error: Invalid switch type" << endl;
+	}
 }
 
 vector<string> SpintaxSwitch::getStringOptions() {
-
-}
-
-void SpintaxSwitch::setSwitchType(string type) {
-
+	return strings;
 }
 
 void SpintaxSwitch::setMinVal(int value) {
@@ -28,10 +85,16 @@ void SpintaxSwitch::setMaxVal(int value) {
 
 }
 
+//returns the input string with the int value in the place of the spintax
 int SpintaxSwitch::spinIntValue() {
 
 }
 
+//returns the input string with the string value in the place of the spintax
 string SpintaxSwitch::spinStringValue() {
 
+}
+
+bool SpintaxSwitch::getNested() {
+	return isNested;
 }
--- spintax/SpintaxSwitch.h
+++ spintax/SpintaxSwitch.h
@@ -1,25 +1,36 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <iostream>
+#include <sstream>
+using namespace std;
+
 class SpintaxSwitch
 {
 private:
 	//TO DO:
 	// Nesting...
-
-	string switch_type;
+	// Global ...
+	// escape characters
+	const int noType = -1;
+	const int intType = 0;
+	const int strType = 1;
+	int switch_type = noType;
 	int minVal;
 	int maxVal;
 	vector<string> strings;
+	string input;
+	bool isNested = false;
 public:
-	string getSwitchType();
+	SpintaxSwitch(string in);
+	int getSwitchType();
 	int getMinVal();
 	int getMaxVal();
 	vector<string> getStringOptions();
-	void setSwitchType(string type);
 	void setMinVal(int value);
 	void setMaxVal(int value);
 	int spinIntValue();
 	string spinStringValue();
+	bool getNested();
 };
 
